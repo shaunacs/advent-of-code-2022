49340e5db6d72c9fc3a3d199daf4b7b9825a1528
Shauna Saunders <shaunacsaunders@outlook.com>
2022-12-02 13:47:02 -0500

    Add win or lose function

---
+++ day-2/part-1.py
@@ -0,0 +1,27 @@
+# A = Rock
+# B = Paper
+# C = Scissors
+
+# X = Rock
+# Y = Paper
+# Z = Scissors
+
+def win_or_lose(opponent, you):
+    if opponent == 'A' and you == 'X':
+        return 'Draw'
+    if opponent == 'A' and you == 'Y':
+        return 'Win'
+    if opponent == 'A' and you == 'Z':
+        return 'Lose'
+    if opponent == 'B' and you == 'X':
+        return 'Lose'
+    if opponent == 'B' and you == 'Y':
+        return 'Draw'
+    if opponent == 'B' and you == 'Z':
+        return 'Win'
+    if opponent == 'C' and you == 'X':
+        return 'Win'
+    if opponent == 'C' and you == 'Y':
+        return 'Lose'
+    if opponent == 'C' and you == 'Z':
+        return 'Draw'
