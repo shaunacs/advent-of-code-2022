4e1929cae529c449a583db401e39fcf84411d305
Shauna Saunders <shaunacsaunders@outlook.com>
2022-12-17 09:54:39 -0500

    Create tree and node classes

--- day-7/day-7.py
+++ day-7/day-7.py
@@ -1,3 +1,30 @@
+class Tree:
+    """Tree."""
+
+    def __init__(self, root):
+        self.root = root
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Tree root={self.root}>"
+
+
+class Node:
+    """Node in a tree."""
+
+    def __init__(self, data, children):
+        self.data = data
+        self.children = children
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Node {self.data}>"
+
+
+
+
 def split_input(input):
     terminal_output = open(input)
     terminal_output_list = []
@@ -12,24 +39,31 @@ def split_command_line(command):
     return command.split()
 
 terminal_output = split_input('./input.txt')
-directories = {}
-location = ""
 
-for line in terminal_output:
-    commands = split_command_line(line)
-    if commands[0] == '$':
-        if commands[1] == 'cd':
-            if location not in directories:
-                directories[commands[2]] = {}
-                location = commands[2]
-        elif commands[1] == 'ls':
-            continue
-    if commands[0] == 'dir':
-        if commands[1] not in directories[location]:
-            directories[location][commands[1]] = {}
-        #else:
-            #directories[location][commands[1]].add()
-    elif commands[0] != '$':
-        directories[location][commands[1]] = commands[0]
+files = Tree(Node('/', []))
+print(files)
 
-    print(directories)
+
+
+
+# directories = {}
+# location = ""
+
+# for line in terminal_output:
+#     commands = split_command_line(line)
+#     if commands[0] == '$':
+#         if commands[1] == 'cd':
+#             if location not in directories:
+#                 directories[commands[2]] = {}
+#                 location = commands[2]
+#         elif commands[1] == 'ls':
+#             continue
+#     if commands[0] == 'dir':
+#         if commands[1] not in directories[location]:
+#             directories[location][commands[1]] = {}
+#         #else:
+#             #directories[location][commands[1]].add()
+#     elif commands[0] != '$':
+#         directories[location][commands[1]] = commands[0]
+
+#     print(directories)
